60442bd3001ebf46e4aff82321eab1c9aba1ce02
Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>
2013-11-12 14:24:09 -0200

    graph.js: Beginning to put the graph code within an object
    Signed-off-by: Diego Giovane Pasqualin <dpasqualin@c3sl.ufpr.br>

--- graph.js
+++ graph.js
@@ -1,45 +1,59 @@
 "use strict";
 
-var width = 960,
-    height = 2200;
+var Graph = function () {
+    this.width =  960;
+    this.height = 2200;
 
-var cluster = d3.layout.cluster()
-    .size([height, width - 160]);
+    this.cluster = d3.layout.cluster()
+                            .size([height, width - 160]);
 
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
+    this.diagonal = d3.svg.diagonal()
+                          .projection(function(d) { return [d.y, d.x]; });
 
-var svg = d3.select("#graph").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(40,0)");
+    this.svg = d3.select("#graph")
+                 .append("svg")
+                     .attr("width", width)
+                     .attr("height", height)
+                 .append("g")
+                     .attr("transform", "translate(40,0)");
 
+    d3.select(self.frameElement).style("height", this.height + "px");
+};
 
-var nodes = cluster.nodes(dataJson),
-    links = cluster.links(nodes);
-
-var link = svg.selectAll(".link")
-              .data(links)
+Graph.prototype._drawEdges = function(edges) {
+    return svg.selectAll(".link")
+              .data(edges)
               .enter().append("path")
               .attr("class", "link")
-              .attr("d", diagonal);
+              .attr("d", this.diagonal);
+};
 
-var node = svg.selectAll(".node")
-              .data(nodes)
+Graph.prototype._drawNodes = function(nodesIn) {
+    var nodes svg.selectAll(".node")
+              .data(nodesIn)
               .enter().append("g")
               .attr("class", "node")
               .attr("transform", function(d) {
-                        return "translate(" + d.y + "," + d.x + ")";
+                    return "translate(" + d.y + "," + d.x + ")";
               });
 
-node.append("circle")
-    .attr("r", 4.5);
+    nodeIn.append("circle")
+          .attr("r", 4.5);
 
-node.append("text")
-    .attr("dx", function(d) { return d.children ? -8 : 8; })
-    .attr("dy", 3)
-    .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-    .text(function(d) { return d.name; });
 
-d3.select(self.frameElement).style("height", height + "px");
+    nodeIn.append("text")
+          .attr("dx", function(d) { return d.children ? -8 : 8; })
+          .attr("dy", 3)
+          .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+          .text(function(d) { return d.name; });
+}
+
+Graph.prototype.draw = function(data) {
+
+    var nodes = cluster.nodes(data),
+        edges = cluster.links(nodes);
+
+    this._drawEdges(edges);
+    this._drawNodes(nodes);
+
+}
