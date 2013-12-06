cd9c7cf4880591eb0128faa05302147e24e66b6c
Diego Pasqualin <dpasqualin@c3sl.ufpr.br>
2013-12-06 17:13:08 -0200

    dpll.js: Remove license info and unnecessary update_graph call

--- dpll.js
+++ dpll.js
@@ -1,32 +1,3 @@
-/*
-The MIT License
-
-Copyright (c) 2010 Mariano M. Chouza
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-/*
-Uses DPLL as described in  Cook and Mitchell "Finding Hard Instances of the
-Satisfiability Problem: A Survey" p. 3
-*/
-
 "use strict";
 
 var Dpll = function(graph, config) {
@@ -170,14 +141,13 @@ Dpll.prototype.solve = function(formula, assignment, config) {
     this._graph.clean();
 
     this._recDPLL(formula, assignment, tree);
+    this._updateGraph();
     if (!this._config.step_by_step) {
         while (this.nextStep()[0] === false) {
             // do nothing
         }
-        this._updateGraph();
         return this.nextStep();
     } else {
-        this._updateGraph();
         return [false, false, {}];
     }
 
